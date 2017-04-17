7f243837cd9196c3fbb418b20d72b99f71d8e81b
user <user@MacBook-Air-user.local>
2017-04-17 14:50:11 +0500

    Add replaceUnicode option.

--- entry.js
+++ entry.js
@@ -1,7 +1,6 @@
 var parse = require('./lib');
 
-var expression = '<div>{a + b}123</div>';
+var expression = '<div>{a + b}123&dagger;Привет!&nbsp;{a + c}</div>';
+var parsed = parse(expression);
 
-console.log(parse(expression, {
-  keepOriginal: true
-}));
+console.log(parsed);
